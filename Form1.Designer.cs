2762084d92b9d11606d9ad2eae1bb12c311327e2
Nazar47Lysyk32 <137522819+Nazar47Lysyk32@users.noreply.github.com>
2024-05-07 14:33:37 +0300

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,84 @@
+﻿namespace WindowsFormsApp5
+{
+    partial class Form1
+    {
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button btnAddTask;
+        private System.Windows.Forms.Button btnRemoveTask;
+        private System.Windows.Forms.DateTimePicker dateTimePicker1;
+        private System.ComponentModel.IContainer components = null;
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        private void InitializeComponent()
+        {
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.btnAddTask = new System.Windows.Forms.Button();
+            this.btnRemoveTask = new System.Windows.Forms.Button();
+            this.dateTimePicker1 = new System.Windows.Forms.DateTimePicker();
+            this.SuspendLayout();
+
+            // listBox1
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(12, 12);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(260, 160);
+            this.listBox1.TabIndex = 0;
+
+            // textBox1
+            this.textBox1.Location = new System.Drawing.Point(12, 180);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(178, 20);
+            this.textBox1.TabIndex = 1;
+
+            // btnAddTask
+            this.btnAddTask.Location = new System.Drawing.Point(196, 178);
+            this.btnAddTask.Name = "btnAddTask";
+            this.btnAddTask.Size = new System.Drawing.Size(75, 23);
+            this.btnAddTask.TabIndex = 2;
+            this.btnAddTask.Text = "Add Task";
+            this.btnAddTask.UseVisualStyleBackColor = true;
+
+            // btnRemoveTask
+            this.btnRemoveTask.Location = new System.Drawing.Point(12, 206);
+            this.btnRemoveTask.Name = "btnRemoveTask";
+            this.btnRemoveTask.Size = new System.Drawing.Size(94, 23);
+            this.btnRemoveTask.TabIndex = 3;
+            this.btnRemoveTask.Text = "Remove Task";
+            this.btnRemoveTask.UseVisualStyleBackColor = true;
+
+            // dateTimePicker1
+            this.dateTimePicker1.Format = System.Windows.Forms.DateTimePickerFormat.Custom;
+            this.dateTimePicker1.CustomFormat = "MM/dd/yyyy HH:mm:ss";
+            this.dateTimePicker1.Location = new System.Drawing.Point(12, 235);
+            this.dateTimePicker1.Name = "dateTimePicker1";
+            this.dateTimePicker1.Size = new System.Drawing.Size(200, 20);
+            this.dateTimePicker1.TabIndex = 4;
+
+            // Form1
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.Controls.Add(this.dateTimePicker1);
+            this.Controls.Add(this.btnRemoveTask);
+            this.Controls.Add(this.btnAddTask);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.listBox1);
+            this.Name = "Form1";
+            this.Text = "Task Reminder App";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+        }
+    }
+
+}
+
---
+++ Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Windows.Forms;
+using System.Collections.Generic;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+
+public partial class Form1 : Form
+{
+    private List<System.Threading.Timer> timers = new List<System.Threading.Timer>();
+
+    public Form1()
+    {
+        InitializeComponent();
+        btnAddTask.Click += new EventHandler(btnAddTask_Click);
+        btnRemoveTask.Click += new EventHandler(btnRemoveTask_Click);
+    }
+
+    private void btnAddTask_Click(object sender, EventArgs e)
+    {
+        if (!string.IsNullOrWhiteSpace(textBox1.Text))
+        {
+            DateTime reminderTime = dateTimePicker1.Value;
+            DateTime now = DateTime.Now;
+
+            if (reminderTime <= now)
+            {
+                MessageBox.Show("Please set the reminder time in the future.");
+                return;
+            }
+
+            string taskDescription = textBox1.Text;
+            listBox1.Items.Add($"{taskDescription} (Reminder set for {reminderTime})");
+            textBox1.Clear();
+
+            TimeSpan delay = reminderTime - now;
+            System.Threading.Timer timer = new System.Threading.Timer(ShowReminder, taskDescription, delay, TimeSpan.FromMilliseconds(-1));
+            timers.Add(timer);
+        }
+        else
+        {
+            MessageBox.Show("Please enter some text for the task.");
+        }
+    }
+
+    private void ShowReminder(object state)
+    {
+        string taskDescription = (string)state;
+        MessageBox.Show("Reminder: " + taskDescription, "Task Reminder", MessageBoxButtons.OK, MessageBoxIcon.Information);
+    }
+
+    private void btnRemoveTask_Click(object sender, EventArgs e)
+    {
+        if (listBox1.SelectedIndex != -1)
+        {
+            listBox1.Items.RemoveAt(listBox1.SelectedIndex);
+        }
+        else
+        {
+            MessageBox.Show("Please select a task to remove.");
+        }
+    }
+}
+
